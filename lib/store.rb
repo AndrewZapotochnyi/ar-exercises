d9be81638c40c7870042b9fbd55e503b2e8ddd51
AndrewZapotochnyi <info@azblockchain.co>
2020-05-26 00:54:42 +0000

    Question 7 - Model validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,8 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates_length_of :name, minimum: 5
+
+  validates_numericality_of :annual_revenue, on: :only_integer
+  validates_numericality_of :annual_revenue, greater_than: 0
 end
